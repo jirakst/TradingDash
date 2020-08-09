b731263dbf73d765776f98e458551ebe85fdeb22
jirakst <jirakst@hotmail.com>
2020-08-09 23:09:32 +0200

    Update __init__.py

--- TradingDash/BinanceAPI/__init__.py
+++ TradingDash/BinanceAPI/__init__.py
@@ -5,8 +5,8 @@ import azure.functions as func
 from binance.client import Client
 from binance import BinanceAPIException
 
-api_key = "xDr5QXtDLVei1IRaq2SNfRRxjNhTjuEOgM0MM9fuYi5Kr7BclHELHVLhR3dhOHuj"
-api_secret = "RroWlQwwIbtdYNNFSc51EcI0u0K8N6T1BxdTTLkmMUgjrzng7FYpNNMeKdS2IaFW"
+api_key = "SECRET API KEY"
+api_secret = "SECRET API KEY"
 client = Client(api_key, api_secret, {"verify": False, "timeout": 20})
 
 def main(mytimer: func.TimerRequest) -> None:
